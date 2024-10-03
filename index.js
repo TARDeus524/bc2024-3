afa2d3d155f099fbea43b2e6330b87c3546b8332
Maksymbc2024-3 <nevmermaks112@gmail.com>
2024-10-03 10:36:21 +0000

    Added read and write from file

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const {program} = require('commander');
 const fs = require('node:fs');
 
+let TotalIncome;
+let TotalSpent;
+
 program
     .option('-i, --input <file>', 'path to input file')
-    .option('-o, --output', 'path to output file')
+    .option('-o, --output [file]', 'path to output file')
     .option('-d, --display', 'result to console');
 
 program.parse(process.argv);
@@ -13,18 +16,30 @@ if(!opts.input){
     throw new Error('Please, specify input file');
 }
 else{
-    fs.readFile(opts.input, 'utf8', (err, data) => {
-        if (err) {
-          throw new Error('Cannot find input file');
-        }
-        
-      });
+    try{
+      let data = fs.readFileSync(opts.input, 'utf8')
+
+        let dataArrObj = JSON.parse(data);
+
+        dataArrObj.forEach(element => {
+          if(element["txten"] == "Income, total") {TotalIncome = element["value"];}
+          if(element["txten"] == "Expenses, total") {TotalSpent = element["value"];}
+
+        });
+
+      }
+      catch(err){
+        throw new Error('Cannot find input file')
+      }
 }
 
 if(opts.output){
-
+  fs.writeFile(opts.output, "Доходи, усього:" + TotalIncome + "\n" +  "Витрати, усього:" + TotalSpent, err => {
+    if(err) {throw new Error("Could not write or create a file");}
+  })
 }
 
 if(opts.display){
-
+  console.log("Доходи, усього:" + TotalIncome);
+  console.log("Витрати, усього:" + TotalSpent);
 }
